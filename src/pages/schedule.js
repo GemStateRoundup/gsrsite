bb165e2b6076680b7a86f3ccbf70a5d241faf102
Mark Long <mark.long@marklong.io>
2019-04-28 11:14:07 -0600

    Added Schedule Component

---
+++ src/pages/schedule.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Header from "../components/Globals/Header"
+
+const Schedule = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <Header />
+  </Layout>
+)
+
+export default Schedule
