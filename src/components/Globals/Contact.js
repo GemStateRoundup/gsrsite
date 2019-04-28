9ab77944e33e818e42fab736792f1c48e0273c9f
Mark Long <mark.long@marklong.io>
2019-04-28 11:13:16 -0600

    Added Contact Component

---
+++ src/components/Globals/Contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Title from "../Globals/Title"
+
+export default function Contact() {
+  return (
+    <section className="pt-5">
+      <div className="row">
+        <div className="col-12 col-sm-10 mx-auto text-center">
+          <div className="container">
+            <Title title="contact & location" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
