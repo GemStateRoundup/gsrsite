6e675c61f4333fb453e89803ac92501c2452527e
Mark Long <mark.long@marklong.io>
2019-06-03 12:51:49 -0600

    Added Custom Fonts to Layout Component

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,6 +14,7 @@ import Footer from "./Globals/Footer"
 
 import "./bootstrap.min.css"
 import "./layout.css"
+import "../fonts/ZaloraInlineBold.ttf"
 
 if (typeof window !== "undefined") {
   // eslint-disable-next-line global-require
