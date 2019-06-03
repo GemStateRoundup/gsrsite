94b31532443c5ffe76955a7fce004d62eb294d3f
Mark Long <mark.long@marklong.io>
2019-06-03 12:50:32 -0600

    Added Styles fo Title Component

--- src/components/Globals/Title.js
+++ src/components/Globals/Title.js
@@ -4,7 +4,7 @@ export default function Title({ title }) {
   return (
     <div className="row">
       <div className="col text-center mb-3">
-        <h1 className="display-4 text-capitalize font-weight-bold mt-5">
+        <h1 className="display-4 text-capitalize font-weight-bold title-header mt-5">
           {title}
         </h1>
       </div>
