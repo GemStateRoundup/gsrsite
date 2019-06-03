8f4659bfa593f16c8f0e62d5b264bb0a819d1291
Mark Long <mark.long@marklong.io>
2019-06-03 12:45:42 -0600

    Changed Header Background Image and Styles

--- src/components/Globals/Header.js
+++ src/components/Globals/Header.js
@@ -4,7 +4,9 @@ export default function Header({ title }) {
   return (
     <div className="row pages">
       <div className="col text-center">
-        <h1 className="display-4 text-capitalize font-weight-bold">{title}</h1>
+        <h1 className="display-4 text-capitalize font-weight-bold text-white header-title">
+          {title}
+        </h1>
       </div>
     </div>
   )
