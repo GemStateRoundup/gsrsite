f13266c39f3705e88431e4362089da16f1bb7e18
Mark Long <mark.long@marklong.io>
2019-06-03 17:16:57 -0600

    Added # to th and Responsive Feature to Tables

--- src/components/Globals/Schedules/Friday.js
+++ src/components/Globals/Schedules/Friday.js
@@ -22,28 +22,30 @@ export default () => (
     render={data => (
       <div>
         <Title title="Friday" />
-        <table className="table table-striped table-hover">
-          <thead className="thead-dark">
-            <tr>
-              <th scope="col" />
-              <th scope="col">Event</th>
-              <th scope="col">Time</th>
-              <th scope="col">Location</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.allContentfulFridayEvents.edges.map(({ node }) => {
-              return (
-                <tr key={node.id}>
-                  <th scope="row">{node.number}</th>
-                  <td>{node.title}</td>
-                  <td>{node.time}</td>
-                  <td>{node.location}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+        <div className="table-responsive-sm">
+          <table className="table table-striped table-hover">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Event</th>
+                <th scope="col">Time</th>
+                <th scope="col">Location</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.allContentfulFridayEvents.edges.map(({ node }) => {
+                return (
+                  <tr key={node.id}>
+                    <th scope="row">{node.number}</th>
+                    <td>{node.title}</td>
+                    <td>{node.time}</td>
+                    <td>{node.location}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     )}
   />
