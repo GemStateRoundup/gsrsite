4d63441f6963897a21344e507dcea4b144cd1655
Mark Long <mark.long@marklong.io>
2019-06-03 12:51:14 -0600

    Modified Padding Footer

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Footer() {
   return (
-    <footer className="footer py-4">
+    <footer className="footer py-5">
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-6 text-capitalize">
